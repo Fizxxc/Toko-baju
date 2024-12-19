ef36140195e49c695f4b9c85c57bfad0e28dad35
Fizxxc <fizzx909@gmail.com>
2024-12-19 22:21:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function welcome() {
+	alert('Selamat datang di Katalog Produk!');
+	window.location.href='katalog.html';
+}
+
+function diskon() {
+	nama = prompt('Siapa nama kamu?');
+
+	alert('Selamat ' + nama + " Kamu mendapatkan voucher 2%!");
+}
