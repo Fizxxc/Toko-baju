0abc6b864156ce4c3d80a195182e5e917016cde0
Fizxxc <fizzx909@gmail.com>
2024-12-20 22:55:44 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 function welcome() {
-	alert('Selamat datang di Katalog Produk!');
 	window.location.href='katalog.html';
 }
 
 function diskon() {
-	nama = prompt('Siapa nama kamu?');
+	Swal.fire({
+		title: 'Siapa nama kamu?',
+		input: 'text',
+		inputPlaceholder: 'Masukkan nama kamu',
+		showCancelButton: true,
+		confirmButtonText: 'Dapatkan Diskon!',
+		cancelButtonText: 'Batal'
+	}).then((result) => {
+		if (result.isConfirmed && result.value) {
+			Swal.fire(
+				'Selamat ' + result.value + '!',
+				'Kamu mendapatkan voucher 2%!',
+				'success'
+			);
+		}
+	});
+}
 
-	alert('Selamat ' + nama + " Kamu mendapatkan voucher 2%!");
+function showWelcomeMessage() {
+	Swal.fire({
+		title: 'Welcome All!',
+		text: 'Selamat datang di website kami.',
+		icon: 'info',
+		confirmButtonText: 'Terima Kasih'
+	});
+}
+
+function showCatalogMessage() {
+	Swal.fire({
+		title: 'Welcome To Catalog!',
+		text: 'Selamat datang di katalog kami.',
+		icon: 'info',
+		confirmButtonText: 'Terima Kasih'
+	});
 }
